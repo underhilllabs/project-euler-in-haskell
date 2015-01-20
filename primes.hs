bb36de42174083fb87b6a1be07d39ddc1782fcb0
Bart Lantz <bartlantz@gmail.com>
2015-01-20 13:19:36 -0700

    Adds utility for first PE question

--- primes.hs
+++ primes.hs
@@ -11,3 +11,14 @@ ldf k n | divides k n = k
 
 prime0 :: Int -> Bool
 prime0 n = ld n == n
+
+-- sum (mult3or5range 999)
+mult3or5range :: Int -> [Int]
+mult3or5range n = [x| x <- [1..n], mult3or5 x]
+
+mult3or5 :: Int -> Bool
+mult3or5 n | rem n 3 == 0 = True
+            | rem n 5 == 0 = True
+            | otherwise = False
+
+
